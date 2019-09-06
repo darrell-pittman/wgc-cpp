3c67446b6d75462804167bd601226cec2ae98e83
Darrell Pittman <darrellppittman@gmail.com>
2019-09-05 22:14:58 -0230

    Some clean up

--- include/wgc/wgc_future.h
+++ include/wgc/wgc_future.h
@@ -9,7 +9,9 @@ namespace wgc
   template<typename T>
   bool FutureReady(const std::future<T>& Future)
   {
-    return Future.valid() && (Future.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready);
+    return 
+      Future.valid() 
+      && (Future.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready);
   }
 
 }
