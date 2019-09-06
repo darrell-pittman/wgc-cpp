a98c233e52177629f31b234feb9fd9c1da55e77f
Darrell Pittman <darrellppittman@gmail.com>
2019-09-05 22:04:13 -0230

    New FutureReady function.

---
+++ include/wgc/wgc_future.h
@@ -0,0 +1,17 @@
+#ifndef WGC_BYTEBUFFER_H
+#define WGC_BYTEBUFFER_H
+
+#include <future>
+#include <chrono>
+
+namespace wgc
+{
+  template<typename T>
+  bool FutureReady(const std::future<T>& Future)
+  {
+    return Future.valid() && (Future.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready);
+  }
+
+}
+
+#endif
